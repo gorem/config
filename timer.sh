ff424fe2316e0b1420dec4553f441ca7d73181df
Mike G <gore@colorado.edu>
2015-11-07 08:00:11 +0100

    Phrase randomizer
    Adapted for public consumption :)

--- timer.sh
+++ timer.sh
@@ -5,5 +5,14 @@ while [ $time_remaining != 0 ]; do
     sleep 60
     let "time_remaining -= 1"
 done
-osascript -e 'display notification "Time is up!" with title "Freedom" sound name "Submarine"'
+title_num=$RANDOM
+body_num=$RANDOM
+
+declare -a titles=("Freedom" "Timer Completed" "It's all over" "Way to be" "Bet you'd like a break" "What time is it?")
+declare -a bodys=("You have completed your task." "Time is up!" "Take a break, Jake!" "You the man!" "What kind of time is it?" "Game time!" "STOP COMPUTING" "yolo")
+
+which_title=$((title_num % ${#titles[@]}))
+which_body=$((body_num % ${#bodys[@]}))
+
+osascript -e 'display notification "'"${bodys[$which_body]}"'" with title "'"${titles[$which_title]}"'" sound name "Submarine"'
 say "Time is up"
