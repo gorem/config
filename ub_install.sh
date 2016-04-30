13b06c63cc36bada4d7d07f6759ac1febdb8c75e
Mike Gore <gore@colorado.edu>
2016-04-30 10:35:53 +0200

    full dir names

--- ub_install.sh
+++ ub_install.sh
@@ -16,8 +16,8 @@ unzip 1.030R-it.zip
 mkdir -p ~/.fonts
 cp source-code-pro-2.010R-ro-1.030R-it/OTF/*.otf ~/.fonts/
 fc-cache -f -v
-rm 1.030R-it.zip
-rm -r source-code-pro-2.010R-ro-1.030R-it/
+rm ~/Downloads/1.030R-it.zip
+rm -r ~/Downloads/source-code-pro-2.010R-ro-1.030R-it/
 
 if [ ! -a ~/.inputrc ]; then echo "\$include /etc/inputrc" > ~/.inputrc; fi
 echo "set completion-ignore-case On" >> ~/.inputrc
