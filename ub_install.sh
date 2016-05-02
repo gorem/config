59ccd65befc62eb8e11fca13ff5e5c21757439d7
Mike G <gore@colorado.edu>
2016-05-02 20:06:13 +0200

    fixed directories

--- ub_install.sh
+++ ub_install.sh
@@ -10,7 +10,7 @@ git clone https://github.com/gorem/config.git
 cp config/ub_bash_profile ~/.bash_profile
 cp config/ub_emacs ~/.emacs
 
-cd Downloads
+cd ~/Downloads
 wget https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
 unzip 1.030R-it.zip
 mkdir -p ~/.fonts
@@ -22,6 +22,7 @@ rm -r source-code-pro-2.010R-ro-1.030R-it/
 if [ ! -a ~/.inputrc ]; then echo "\$include /etc/inputrc" > ~/.inputrc; fi
 echo "set completion-ignore-case On" >> ~/.inputrc
 
+# Spotify - not for VMs
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
