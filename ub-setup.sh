52f9c3c5062e910ad47868eff2b4d0c36f2a9a75
Mike Gore <gore@colorado.edu>
2016-05-24 21:06:44 +0200

    Fix link to setup script in script
    Changing the file name changed the link on GitHub. I have
    really spent a lot of time on this script.

--- ub-setup.sh
+++ ub-setup.sh
@@ -48,4 +48,4 @@ if [ "$want_spotify" = true ]; then
     exit;
 fi
 
-# wget https://raw.githubusercontent.com/gorem/config/master/ub_install.sh
+# wget https://raw.githubusercontent.com/gorem/config/master/ub-setup.sh
