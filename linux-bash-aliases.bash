e48f740467bd13191ad33f1bdc411d2d59d0b33c
Mike Gore <gore@colorado.edu>
2018-02-11 22:01:11 -0700

    Update Linux ~/.bash_aliases
    Grab common commands like fr, and make it match what I actually
    have on my system so I can just copy and commit it in the future.

--- linux-bash-aliases.bash
+++ linux-bash-aliases.bash
@@ -4,18 +4,21 @@ alias e="emacs"
 alias em="emacs"
 alias et="emacs -nw"
 alias xe="xelatex"
+alias qu="shuf -n 1 ~/Dropbox/read/quotes.txt"
+alias cl="clear"
+alias fr="cd ~ && clear"
+function quad {
+	if [[ $@ ]]; then
+		for var in "$@"
+		do
+			grep -ri --color=auto "$var" "/home/mike/Code/Scriptures"
+		done
+	else
+		cd ~/Code/Scriptures/
+	fi
+}
 function jr {
     yr=`date +"%Y"`
     mthyr=`date +"%B %Y"`
     emacs -nw ~/Dropbox/write/journal/"$yr"/"$mthyr".txt --eval "(progn (goto-char (point-max)) (insert-journal-timestamp))"
-}
-function kp {
-    if [[ $@ ]]; then
-       for var in "$@"
-       do
-           echo "$@" >> ~/Dropbox/write/keep.txt
-       done
-    else
-       et ~/Dropbox/write/keep.txt
-    fi
 }
