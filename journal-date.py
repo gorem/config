7febde99162d43a8eb4e012e97e2b25bcb51e193
Mike Gore <gorem@users.noreply.github.com>
2019-05-15 22:24:53 -0600

    Use p.m. starting at noon
    Previously used p.m. after 1 o'clock in the afternoon.
    Previously this would give the incorrect suffix for, say, 12:30.
    According to a few seconds Googling, there is some
    debate around which to use at noon exactly (a.m. or p.m.).

--- journal-date.py
+++ journal-date.py
@@ -5,7 +5,7 @@ import datetime
 today = datetime.datetime.now()
 
 # Could use %p in strftime below, but that uses AM/PM
-if today.hour > 12:
+if today.hour >= 12:
     suffix = "p.m."
 else:
     suffix = "a.m."
