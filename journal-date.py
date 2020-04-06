259d5bbc87d9de9226bf6b361fc41db41cde167b
Mike Gore <gore@colorado.edu>
2020-04-05 19:43:56 -0600

    Simplify journal-date

--- journal-date.py
+++ journal-date.py
@@ -5,10 +5,9 @@ import datetime
 today = datetime.datetime.now()
 
 # Could use %p in strftime below, but that uses AM/PM
+suffix = "a.m."
 if today.hour >= 12:
     suffix = "p.m."
-else:
-    suffix = "a.m."
 
 print(today.strftime("%A, %B %-d, %Y"))
-print(today.strftime("%-I:%M") + " " + suffix + "\n")
+print(today.strftime("%-I:%M ") + suffix + "\n")
