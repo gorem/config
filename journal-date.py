2b267ad035cb132ded22ec5e628f4720170d1745
Mike Gore <gore@colorado.edu>
2020-04-05 20:10:47 -0600

    Add location to Mac journal-date script

--- journal-date.py
+++ journal-date.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import datetime
+import os
 
 today = datetime.datetime.now()
 
@@ -9,5 +10,18 @@ suffix = "a.m."
 if today.hour >= 12:
     suffix = "p.m."
 
+location_command = '''CoreLocationCLI -once -format "%locality^%administrativeArea^%country"'''
+location_string = os.popen(location_command).read()
+location_data = location_string[:-1].split("^")
+city = location_data[0]
+state = location_data[1]
+country = location_data[2]
+if country == "United States":
+    country = "U.S.A."
+
+if today.day != 1:
+    print("\n")
 print(today.strftime("%A, %B %-d, %Y"))
-print(today.strftime("%-I:%M ") + suffix + "\n")
+print(today.strftime("%-I:%M ") + suffix)
+print(city + ", " + state + ", " + country)
+print("")
