bbfcbad8fa63ec313a23c8cad7d19cf8721e9edc
Mike Gore <gore@colorado.edu>
2021-01-12 22:03:25 -0700

    Create wsl-journal.py
    Add script for entering journal date on WSL.

---
+++ wsl-journal.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+import datetime
+
+today = datetime.datetime.now()
+
+# Could use %p in strftime below, but that uses AM/PM
+suffix = "a.m."
+if today.hour >= 12:
+    suffix = "p.m."
+
+print("\n")
+print(today.strftime("%A, %B %-d, %Y"))
+print(today.strftime("%-I:%M ") + suffix)
+print("Denver, CO, U.S.A.\n")
