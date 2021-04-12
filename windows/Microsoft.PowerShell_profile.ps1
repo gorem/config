0f115e342ee353ea9560a02b2c662ef2bda0cab0
Mike Gore <gore@colorado.edu>
2021-04-11 21:29:31 -0600

    Update folder location to E drive
    Use the correct location now that I have a third internal disk.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 function Write-Journal {
     # Set variables
-    $JournalDir = 'D:\Mike\iCloud Drive\27N4MQEA55~pro~writer\'
+    $JournalDir = 'E:\Mike\iCloud Drive\27N4MQEA55~pro~writer\'
     $MonthYear = Get-Date -Format "MMMM yyyy"
     $JournalFile = Join-Path -Path "$JournalDir" -ChildPath "$MonthYear.txt"
     # Append today's date
-    python D:\Mike\Code\config\windows-journal.py | Out-File -FilePath $JournalFile -Append -en utf8
+    python E:\Mike\Code\config\windows-journal.py | Out-File -FilePath $JournalFile -Append -en utf8
     # Open journal in Visual Studio Code at end of file
     code -g ($JournalFile + ':500')
 }
