9863f731c0565c7729d6e811c5469236c540bf61
Mike Gore <gore@colorado.edu>
2021-04-12 21:06:43 -0600

    Point to new script location
    Should have caught this with the reorganization before.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,7 @@ function Write-Journal {
     $MonthYear = Get-Date -Format "MMMM yyyy"
     $JournalFile = Join-Path -Path "$JournalDir" -ChildPath "$MonthYear.txt"
     # Append today's date
-    python E:\Mike\Code\config\windows-journal.py | Out-File -FilePath $JournalFile -Append -en utf8
+    python E:\Mike\Code\config\windows\windows-journal.py | Out-File -FilePath $JournalFile -Append -en utf8
     # Open journal in Visual Studio Code at end of file
     code -g ($JournalFile + ':500')
 }
